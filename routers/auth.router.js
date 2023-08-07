fafb9e5819fd0e0666c7df4d58b068230f2db3a2
Gsousaz <doomlighthighlord@gmail.com>
2023-08-07 12:55:07 -0300

    fix: signup

--- routers/auth.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
-
-const authRouter = Router();
-
-authRouter.get("/games", signup);
-authRouter.post("/games", signin);
-
-export default authRouter;
